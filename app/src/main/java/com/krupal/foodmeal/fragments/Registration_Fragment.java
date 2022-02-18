cf1fc011678b26a3d05ec12eaf575d0724753b3a
20may-legion <vbviradiya02@gmail.com>
2022-02-18 11:57:15 +0530

    Registration code done with data added to database

--- app/src/main/java/com/krupal/foodmeal/fragments/Registration_Fragment.java
+++ app/src/main/java/com/krupal/foodmeal/fragments/Registration_Fragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -17,9 +18,12 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.krupal.foodmeal.Authentication;
 import com.krupal.foodmeal.R;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -29,7 +33,7 @@ import butterknife.ButterKnife;
 public class Registration_Fragment extends Fragment {
     private FirebaseAuth firebaseAuth;
     Authentication parent_activity;
-
+    FirebaseFirestore firestore;
     @BindView(R.id.have_acc_TV)
     TextView have_acc;
 
@@ -71,9 +75,8 @@ public class Registration_Fragment extends Fragment {
         ButterKnife.bind(this, view);
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).hide();
         FirebaseApp.initializeApp(parent_activity.getApplicationContext());
-
         firebaseAuth = FirebaseAuth.getInstance();
-
+        firestore = FirebaseFirestore.getInstance();
 
         have_acc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,7 +101,25 @@ public class Registration_Fragment extends Fragment {
         firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-                System.out.println("result"+authResult.toString());
+                Map<String,Object> user = new HashMap<>();
+                user.put("first name", fname_ET.getText().toString().trim());
+                user.put("last name", lname_ET.getText().toString().trim());
+                user.put("email id", email_ET.getText().toString().trim());
+                user.put("mobile no", mobile_ET.getText().toString().trim());
+                user.put("password", password_ET.getText().toString().trim());
+                String uid = Objects.requireNonNull(authResult.getUser()).getUid();
+
+                firestore.collection("User").document(uid).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        showToast("user registration successful");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -106,6 +127,11 @@ public class Registration_Fragment extends Fragment {
 
             }
         });
+
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(parent_activity, message, Toast.LENGTH_LONG).show();
 
     }
 }
